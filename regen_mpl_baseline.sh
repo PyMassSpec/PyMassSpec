cdf16b2f495a433bd713138b3fb7b2ca9cc92ff1
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-02-19 11:48:07 +0000

    Change image hash version thresholds.

--- regen_mpl_baseline.sh
+++ regen_mpl_baseline.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-tox -e py36 -- --mpl-generate-path=tests/baseline
+tox -e py36-display -- --mpl-generate-path=tests/baseline
 
-tox -e py36 -- --mpl-generate-hash-library=image_hashes_36.json
-tox -e py37 -- --mpl-generate-hash-library=image_hashes_37.json
-tox -e py38 -- --mpl-generate-hash-library=image_hashes_38.json
-tox -e py39 -- --mpl-generate-hash-library=image_hashes.json
-tox -e py313 -- --mpl-generate-hash-library=image_hashes_313.json
+tox -e py36-display -- --mpl-generate-hash-library=image_hashes_36.json
+tox -e py37-display -- --mpl-generate-hash-library=image_hashes_37.json
+tox -e py38-display -- --mpl-generate-hash-library=image_hashes_38.json
+tox -e py39-display -- --mpl-generate-hash-library=image_hashes.json
+tox -e py310-display -- --mpl-generate-hash-library=image_hashes_313.json
 
 python3 sort_hashes_json.py
