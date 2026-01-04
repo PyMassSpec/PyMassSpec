5deb3cfc962bdf1634accc2995d56811c4808f67
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-01-04 10:22:10 +0000

    Disable image hash comparison - use baseline images only.

--- tests/test_Display.py
+++ tests/test_Display.py
@@ -38,26 +38,9 @@ from pyms.Spectrum import MassSpectrum
 # this package
 from .constants import *
 
-if sys.version_info[:2] == (3, 6):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_36.json")
-elif sys.version_info[:2] == (3, 7):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_37.json")
-elif sys.version_info[:2] == (3, 8):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_38.json")
-elif sys.version_info >= (3, 10):
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes_313.json")
-else:
-	baseline_dir = str(PathPlus(__file__).parent / "baseline")
-	image_hashes = str(PathPlus(__file__).parent / "image_hashes.json")
-
 check_images = pytest.mark.mpl_image_compare(
-		baseline_dir=baseline_dir,
+		baseline_dir = str(PathPlus(__file__).parent / "baseline"),
 		savefig_kwargs={"dpi": 600},
-		hash_library=image_hashes,
 		backend="agg",
 		)
 
